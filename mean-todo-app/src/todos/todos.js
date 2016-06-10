3be1cd858db34559aaa6d94715cc62a395eb0881
K K <K K>
2016-06-09 21:22:23 -0400

    Added edit,save,cancle button functionality

--- mean-todo-app/src/todos/todos.js
+++ mean-todo-app/src/todos/todos.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 export default function($scope){
 	let params={
 		createHasInput:false
@@ -5,25 +6,47 @@ export default function($scope){
 	$scope.todos =[
 	{
 		task:'Wash Dishes',
-		isCompleted:false
+		isCompleted:false,
+		isEditable:false
 	},
 	{
 		task:'Do Coding',
-		isCompleted:true
+		isCompleted:true,
+		isEditable:false
 	}];
 
-	$scope.onCompletedClick =todo => {
+	$scope.onCompletedClick=todo=> {
 		todo.isCompleted = !todo.isCompleted;
 	};
 
-	$scope.createTask =() =>{
+	$scope.createTask=()=>{
 		params.createHasInput=false;
 		$scope.createTaskInput='';
 
-	}
+	};
+
+	$scope.onEditClick=todo=>{
+			todo.isEditable=true;
+			todo.updatedTask=todo.task;
+
+	};
+
+	$scope.onCancleClick=todo=>{
+		todo.isEditable=false;
+	};
+
+	$scope.updateTask=todo=>{
+		todo.task=todo.updatedTask;
+		todo.isEditable=false;	
+	};
+
+	$scope.removeTask=todoToRemove=>{
+		_.remove($scope.todos ,todo => todo.task === todoToRemove.task);
+
+	};
 
 
-	$scope.$watch('createTaskInput',val =>{
+	$scope.$watch('createTaskInput',val=>{
 		if(!val && params.createHasInput){
 			$scope.todos.pop();
 			params.createHasInput=false;
